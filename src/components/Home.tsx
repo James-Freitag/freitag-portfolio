4636dff4c960a11e77b67cb21acff09e6777e012
james-freitag <jamesfreitag05@gmail.com>
2024-02-28 17:55:28 -0600

    Removed Video

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,5 @@
 import profilePic from "../../public/profilePic.jpg";
-import animatedBg from "../../src/assets/animatedBG.mov";
+// import animatedBg from "../../src/assets/animatedBG.mov";
 
 const Home = () => {
   return (
@@ -7,13 +7,13 @@ const Home = () => {
       id="/"
       className="grid grid-cols-1 md:grid-cols-5 gap-4 px-4 md:px-8 h-screen w-full mx-auto bg-black/15"
     >
-      <video
+      {/* <video
         src={animatedBg}
         loop
         autoPlay
         muted
         className=" absolute object-cover z-[-99999] left-0 w-full h-full"
-      />
+      /> */}
       <h1
         id="text-shadow"
         className="md:col-span-3 text-[4rem] md:text-[7rem] lg:text-[10rem] text-white font-extrabold h-full pl-0 md:pl-12 pb-8 md:pb-0 flex flex-wrap break-all items-center leading-none tracking-wider ease-in duration-200"
